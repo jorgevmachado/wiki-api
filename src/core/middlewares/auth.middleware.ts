592791f467ac4f620dbba5d2614b035ba464f566
Jorge Machado <jorge.vmachado@hotmail.com>
2022-03-16 19:42:35 -0300

    update validate token

--- src/core/middlewares/auth.middleware.ts
+++ src/core/middlewares/auth.middleware.ts
@@ -16,7 +16,12 @@ export default function isAuthenticated(
     const [, token] = authHeader.split(' ');
     try {
         const decodedToken = verify(token, authConfig.jwt.secret as Secret);
-        const { sub } = decodedToken as ITokenPayload;
+        const { sub, exp } = decodedToken as ITokenPayload;
+        const dateExp = new Date(exp * 1000);
+        const dateNow = new Date();
+        if(dateExp < dateNow) {
+            throw new AppError('JWT Token expired.', 401);
+        }
         request.user = { id: sub };
         return next();
     } catch {
